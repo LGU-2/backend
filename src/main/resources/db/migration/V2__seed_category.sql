c7c7201c3959aeec0d94503e45e1e081aa41899d
JAEUNGCHOI <jaeung0129@gmail.com>
2026-08-18 01:04:45 +0900

    [Feat] 확정 카테고리 5개를 초기 데이터로 심는다

---
+++ src/main/resources/db/migration/V2__seed_category.sql
@@ -0,0 +1,6 @@
+INSERT INTO category (name, created_at, updated_at) VALUES
+('수산물', NOW(6), NOW(6)),
+('육류',   NOW(6), NOW(6)),
+('채소',   NOW(6), NOW(6)),
+('과일',   NOW(6), NOW(6)),
+('유제품', NOW(6), NOW(6));
