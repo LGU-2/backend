d9a9b373553ba176c18021e3ec8f6e99b7afbf79
JAEUNGCHOI <jaeung0129@gmail.com>
2026-08-20 23:07:43 +0900

    [Comment] 로트 리포지토리와 상품 API 구현체에 설명을 붙인다

--- src/main/java/com/freshmarket/stock/domain/repository/StockLotRepository.java
+++ src/main/java/com/freshmarket/stock/domain/repository/StockLotRepository.java
@@ -3,5 +3,6 @@ package com.freshmarket.stock.domain.repository;
 import com.freshmarket.stock.domain.entity.StockLot;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// 로트(StockLot) 기본 CRUD
 public interface StockLotRepository extends JpaRepository<StockLot, Long> {
 }
--- src/main/java/com/freshmarket/stock/domain/repository/StockMovementRepository.java
+++ src/main/java/com/freshmarket/stock/domain/repository/StockMovementRepository.java
@@ -3,5 +3,6 @@ package com.freshmarket.stock.domain.repository;
 import com.freshmarket.stock.domain.entity.StockMovement;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// 재고 변동 이력(StockMovement) 기본 CRUD
 public interface StockMovementRepository extends JpaRepository<StockMovement, Long> {
 }
